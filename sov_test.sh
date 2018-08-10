e534f3044f8831d59c46ea5214d7f2c7332a8e06
Robert <36878317+robbowz@users.noreply.github.com>
2018-08-10 13:25:27 +0200

    Update sov_test.sh

--- sov_test.sh
+++ sov_test.sh
@@ -1,6 +1,6 @@
 # sovcoin masternode install script
 # Edited by Robbowz
-VERSION="0.4"
+VERSION="0.5"
 NODEPORT='11888'
 RPCPORT='3322'
 
@@ -29,14 +29,14 @@ function print_info() {
 function install_packages() {
 	cd ~
 	echo "Install packages..."
-	add-apt-repository -yu ppa:bitcoin/bitcoin  &>> ${SCRIPT_LOGFILE}
-	apt-get -y update &>> ${SCRIPT_LOGFILE}
-	apt-get install libzmq3-dev &>> ${SCRIPT_LOGFILE}
+	sudo add-apt-repository -yu ppa:bitcoin/bitcoin  &>> ${SCRIPT_LOGFILE}
+	sudo apt-get -y update &>> ${SCRIPT_LOGFILE}
+	sudo apt-get install libzmq3-dev &>> ${SCRIPT_LOGFILE}
   	sudo apt-get install p7zip-full &>> ${SCRIPT_LOGFILE}
-	apt-get -y install wget make automake autoconf build-essential libtool autotools-dev \
-	git nano python-virtualenv pwgen virtualenv \
-	pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common \
-	libboost-all-dev libminiupnpc-dev libdb4.8-dev libdb4.8++-dev &>> ${SCRIPT_LOGFILE}
+	sudo apt-get -y install wget make automake autoconf build-essential libtool autotools-dev \
+	sudo git nano python-virtualenv pwgen virtualenv \
+	sudo pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common \
+	sudo libboost-all-dev libminiupnpc-dev libdb4.8-dev libdb4.8++-dev &>> ${SCRIPT_LOGFILE}
 	echo "Install done..."
 }
 
